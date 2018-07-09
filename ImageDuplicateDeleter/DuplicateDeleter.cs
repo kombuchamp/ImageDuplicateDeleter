a3716f02c3a3f5417c156016c97bacd96cd1d43c
Konstantin Belousov <kombuchamp@gmail.com>
2018-07-10 02:29:15 +0300

    Initial commit

---
+++ ImageDuplicateDeleter/DuplicateDeleter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace ImageDuplicateDeleter
+{
+    public class DuplicateDeleter
+    {
+        public List<string> imagePaths; // public for tests!
+        private readonly ImageConverter imageConverter = new ImageConverter();
+        //private readonly SHA256Managed hasher = new SHA256Managed(); // Hashing algorithm has bugs atm
+
+        public DuplicateDeleter()
+        {
+            this.imagePaths = Directory.GetFiles(@".\", @"*.jpg").ToList(); // TODO: add options here
+        }
+
+        public void Delete()
+        {
+            Image pic1;
+            Image pic2;
+
+            List<string> filesToDelete = new List<string>();
+
+            while (imagePaths.Count != 0)
+            {
+                using (pic1 = Image.FromFile(imagePaths[0]))
+                {
+
+                    for (int i = 1; i < imagePaths.Count; i++)
+                    {
+                        using (pic2 = Image.FromFile(imagePaths[i]))
+                        {
+                            if (IsDuplicateImage(pic1, pic2))
+                            {
+                                Console.WriteLine($"Found duplicate: {imagePaths[i]}");
+
+                                filesToDelete.Add(imagePaths[i]);
+                                imagePaths.Remove(imagePaths[i]);
+                            }
+                        }
+
+                    }
+
+                    imagePaths.RemoveAt(0); // file at this index is already checked
+
+                    pic1.Dispose();
+                }
+            };
+
+
+
+            //Deletion routine
+            foreach (var item in filesToDelete)
+            {
+                try
+                {
+                    File.Delete(item);
+                }
+                catch (IOException)
+                {
+                    throw;
+                }
+            }
+        }
+
+        // Compares two images and returns true if they are equal
+        public bool IsDuplicateImage(Image pic1, Image pic2)  // public for tests!
+        {
+            if (pic1.Size != pic2.Size)
+                return false;
+
+            // Convert images to byte arrays
+            byte[] rawPic1 = (byte[])imageConverter.ConvertTo(pic1, typeof(byte[]));
+            byte[] rawPic2 = (byte[])imageConverter.ConvertTo(pic2, typeof(byte[]));
+
+            // TODO: This method of comparation won't work in some cases. Consider if its needed at all
+
+            // Generate hash for each image
+            //var hash1 = hasher.ComputeHash(rawPic1);
+            //var hash2 = hasher.ComputeHash(rawPic1);
+
+            return rawPic1.SequenceEqual(rawPic2);
+        }
+    }
+}
+
---
+++ ImageDuplicateDeleter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageDuplicateDeleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deleter = new DuplicateDeleter();
+
+            foreach (var item in deleter.imagePaths)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(new string('-', 50));
+
+            try
+            {
+                deleter.Delete();
+            }
+            catch (Exception e)
+            {
+                var defaultColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Couldnt delete the file: \n({e.Message})\n");
+                Console.ForegroundColor = defaultColor;
+            }
+
+            Console.ReadKey(); // Delay on exit
+        }
+    }
+}

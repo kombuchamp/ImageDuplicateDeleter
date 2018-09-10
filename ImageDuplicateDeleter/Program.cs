90e97cc4c2c5070a3e274157f92441d46fa3e625
Konstantin Belousov <kombuchamp@gmail.com>
2018-09-10 22:04:34 +0300

    console interface

--- ImageDuplicateDeleter/Program.cs
+++ ImageDuplicateDeleter/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ImageDuplicateDeleter
@@ -11,28 +13,73 @@ namespace ImageDuplicateDeleter
     {
         static void Main(string[] args)
         {
-            var deleter = new DuplicateDeleter();
+            if (args.Length == 0)
+            {
+                FindAndDeleteDuplicates();
+            }
+            else if (args.Length == 1 && PathIsValid(args[0]))
+            {
+                FindAndDeleteDuplicates(args[0]);
+            }
+            else
+                ShowHelp();
+        }
 
-            foreach (var item in deleter.imagePaths)
+
+        static void FindAndDeleteDuplicates(string path = "")
+        {
+            var deleter = new DuplicateDeleter();
+            var duplicates = deleter.FindDuplicates();
+
+            if (duplicates.ToList().Count == 0)
+            {
+                Console.WriteLine("No duplicates found.");
+                return;
+            }
+
+            Console.WriteLine("Duplicates found:\n");
+            foreach (var item in duplicates)
             {
                 Console.WriteLine(item);
             }
 
-            Console.WriteLine(new string('-', 50));
+            Console.Write("\nDelete duplicates? (y/n)");
+            var keyPressed = Console.ReadKey();
 
-            try
+            switch (keyPressed.Key)
             {
-                deleter.DeleteFiles( deleter.FindDuplicates() );
-            }
-            catch (Exception e)
-            {
-                var defaultColor = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Couldnt delete the file: \n({e.Message})\n");
-                Console.ForegroundColor = defaultColor;
+                case ConsoleKey.Y:
+                    try
+                    {
+                        deleter.DeleteFiles(duplicates);
+                    }
+                    catch (Exception e)
+                    {
+                        var defaultColor = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Couldnt delete the file: \n({e.Message})\n");
+                        Console.ForegroundColor = defaultColor;
+                    }
+                    Console.WriteLine("\nDuplicates have been deleted.");
+                    break;
+                case ConsoleKey.N:
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid command");
+                    break;
             }
 
-            Console.ReadKey(); // Delay on exit
+        }
+
+        private static bool PathIsValid(string path)
+        {
+            path = Regex.Replace(path, "[\"]", "");
+            return Directory.Exists(path);
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine(res.Help.help);
         }
     }
 }
